6da0578719045be24f6c3082d8ae94cd2cc56344
Nikola <88djony@gmail.com>
2021-07-27 10:58:58 -0700

    check database function added

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let db;
 const request = indexedDB.open('budget', 1);
 
@@ -17,4 +19,28 @@ request.onerror = function(event) {
     console.log("Woops! " + event.target.errorCode)
 };
 
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
 
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch("/api/trasanction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    };
+}
